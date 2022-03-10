bb714de094b62f74a068cfb21c6c7b0689a2be94
lyusk8 <lyusk8@gmail.com>
2022-03-10 09:36:48 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/elias/dsmovie/entities/Score.java
@@ -0,0 +1,28 @@
+package com.elias.dsmovie.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@Setter@Getter
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+    @EmbeddedId
+    private ScorePK id = new ScorePK();
+    private Double value;
+
+    public void setMovie(Movie movie){
+        id.setMovie(movie);
+    }
+
+    public void setUser(User user){
+        id.setUser(user);
+    }
+}
---
+++ backend/src/main/java/com/elias/dsmovie/entities/ScorePK.java
@@ -0,0 +1,25 @@
+package com.elias.dsmovie.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@AllArgsConstructor@NoArgsConstructor
+@Setter@Getter
+@Embeddable
+public class ScorePK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
